40b0a12c48439b8ca8276c8e34c5dd9468408918
LAPTOP18TRIEU-D\ACER\Github <hoangfackerzz01@gmail.com>
2024-01-12 08:07:09 +0700

    ket noi sqlite room commit lan 2

--- app/data/build.gradle.kts
+++ app/data/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "org.duchoang.sqlite"
+    namespace = "org.duchoang.data"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "org.duchoang.sqlite"
+        applicationId = "org.duchoang.data"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
@@ -29,13 +29,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.room:room-runtime:2.4.0")
-    annotationProcessor("androidx.room:room-compiler:2.4.0")
-    implementation("androidx.room:room-common:2.6.1")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.recyclerview:recyclerview:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
